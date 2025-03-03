30d4413d5e6df1806b0841c9550e8013e815829f
Farhad Hossen <farhadhossen2590@gmail.com>
2025-03-03 23:06:43 +0600

    Updates

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDi36Y7Mv5ELuwvrzWcFrW6GpIJuyFYwK0",
+  authDomain: "traveltrade-4f50d.firebaseapp.com",
+  projectId: "traveltrade-4f50d",
+  storageBucket: "traveltrade-4f50d.firebasestorage.app",
+  messagingSenderId: "355231018165",
+  appId: "1:355231018165:web:3e98b2f4a2de80d22722fc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
